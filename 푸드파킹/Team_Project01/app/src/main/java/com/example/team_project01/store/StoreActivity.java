9249714419da5f4f2a9ad7999d2e09a97f61e8a2
b-lightyear <tlsgksthf15@gmail.com>
2022-09-15 12:44:42 +0900

    수정_최종_최종_진짜최종임

--- 푸드파킹/Team_Project01/app/src/main/java/com/example/team_project01/store/StoreActivity.java
+++ 푸드파킹/Team_Project01/app/src/main/java/com/example/team_project01/store/StoreActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -68,8 +69,23 @@ public class StoreActivity extends AppCompatActivity {
 
             }
         });
+
+        //뒤로가기 버튼 추가
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
+    //뒤로가기 버튼 활성화
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     //탭 - 메뉴
     public ArrayList<StoreMenuDTO> menu_list() {
