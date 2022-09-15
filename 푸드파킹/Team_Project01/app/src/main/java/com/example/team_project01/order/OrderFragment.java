d08e6b0a31fe8d1e6c0f71882cdd2c710df299ce
SinSungBum <ssb97015364@gmail.com>
2022-09-15 12:27:32 +0900

    성범수정 09/15

--- 푸드파킹/Team_Project01/app/src/main/java/com/example/team_project01/order/OrderFragment.java
+++ 푸드파킹/Team_Project01/app/src/main/java/com/example/team_project01/order/OrderFragment.java
@@ -1,24 +1,36 @@
 package com.example.team_project01.order;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.team_project01.ListActivity;
 import com.example.team_project01.R;
 
 
 public class OrderFragment extends Fragment {
-
+    CardView order;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
             View v =  inflater.inflate(R.layout.fragment_order, container, false);
+            order = v.findViewById(R.id.order);
+
+            order.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(inflater.getContext(), ListActivity.class);
+                    startActivity(intent);
+                }
+            });
         return v;
     }
 }
