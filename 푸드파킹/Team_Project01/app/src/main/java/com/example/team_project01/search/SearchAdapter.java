9bdabb7a9adeb69ad33b6ca27dc42ab78aaa1a8e
b-lightyear <tlsgksthf15@gmail.com>
2022-09-15 12:38:29 +0900

    검색 안 된거 추가

--- 푸드파킹/Team_Project01/app/src/main/java/com/example/team_project01/search/SearchAdapter.java
+++ 푸드파킹/Team_Project01/app/src/main/java/com/example/team_project01/search/SearchAdapter.java
@@ -1,64 +1,66 @@
 package com.example.team_project01.search;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.team_project01.R;
 
 import java.util.ArrayList;
 
-public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder>{
+public class SearchAdapter extends BaseAdapter {
 
+    ArrayList<SearchDTO> sample;
     LayoutInflater inflater;
-    ArrayList<CategoryDTO> list;
+    String newText;
 
-    public SearchAdapter(LayoutInflater inflater, ArrayList<CategoryDTO> list) {
+    public SearchAdapter(ArrayList<SearchDTO> sample, LayoutInflater inflater, String newText) {
+        this.sample = sample;
         this.inflater = inflater;
-        this.list = list;
-    }
-
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ViewHolder(inflater.inflate(R.layout.item_searach, parent, false));
+        this.newText = newText;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.bind(holder, position);
+    public int getCount() {
+        return sample.size();
     }
 
     @Override
-    public int getItemCount() {
-        return list.size();
+    public Object getItem(int i) {
+        return sample.get(i);
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
 
-        ImageView imgv_category1, imgv_category2;
-        TextView tv_category1, tv_category2;
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+        convertView = inflater.inflate(R.layout.item_search_list, parent, false);
 
-        public ViewHolder(@NonNull View v) {
-            super(v);
+        TextView tv_search = convertView.findViewById(R.id.tv_search);
 
-            imgv_category1 = v.findViewById(R.id.imgv_category1);
-            imgv_category2 = v.findViewById(R.id.imgv_category2);
-            tv_category1 = v.findViewById(R.id.tv_category1);
-            tv_category2 = v.findViewById(R.id.tv_category2);
+        //검색어와 일치한 문자열만 색 바꾸기
+        String content = sample.get(i).getString();
+        SpannableString spannableString = new SpannableString(content);
 
-        }
+        int begin = content.indexOf(newText);
+        int end = begin + newText.length();
 
-        public void bind(@NonNull ViewHolder vh, int i) {
-            vh.imgv_category1.setImageResource(list.get(i).getImgv_category1());
-            vh.imgv_category2.setImageResource(list.get(i).getImgv_category2());
-            vh.tv_category1.setText(list.get(i).getTv_category1());
-            vh.tv_category2.setText(list.get(i).getTv_category2());
-        }
+        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#F25C05")), begin, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new StyleSpan(Typeface.BOLD), begin, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        tv_search.setText(spannableString);
+
+        return convertView;
     }
 }
